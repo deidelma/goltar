c0c380d510b20cc6ccaf1effe11a9b17b23d2294
David Eidelman <Eidelman.David@gmail.com>
2020-11-01 17:53:03 -0500

    parse testing

--- pubmed/pubmed_test.go
+++ pubmed/pubmed_test.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/antchfx/xmlquery"
 	xj "github.com/basgys/goxml2json"
 )
 
@@ -31,4 +32,28 @@ func TestConvertFileToJson2(t *testing.T) {
 	json, err := xj.Convert(xml)
 	check(err)
 	fmt.Println(json.String())
+}
+
+func TestFindPMID(t *testing.T) {
+	data, err := ioutil.ReadFile("testdata/asthma_article_one.xml")
+	check(err)
+	xml := strings.NewReader(string(data))
+	doc, err := xmlquery.Parse(xml)
+	check(err)
+	pmid, err := xmlquery.Query(doc, "//PMID")
+	check(err)
+	fmt.Printf("PMID:%s\n", pmid.InnerText())
+	atitle, err := xmlquery.Query(doc, "//ArticleTitle")
+	check(err)
+	if atitle != nil {
+		fmt.Printf("ArticleTitle:%s\n", atitle.InnerText())
+	}
+	aText, err := xmlquery.QueryAll(doc, "//AbstractText")
+	check(err)
+	if aText == nil {
+		return
+	}
+	for _, t := range aText {
+		fmt.Printf("abstract text element: %s\n", t.InnerText())
+	}
 }
