50d3bb2419246a441b29933423f498de8b2f8435
David Eidelman <Eidelman.David@gmail.com>
2020-11-01 20:59:08 -0500

    Completed pubmed.XMLParse

--- pubmed/pubmed.go
+++ pubmed/pubmed.go
@@ -1,5 +1,14 @@
 package pubmed
 
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/antchfx/xmlquery"
+	"github.com/deidelma/goltar/dbg"
+)
+
 // pubmed exports functionality that allows the parsing of an
 // xml file into a Article struct and provides for it translation
 // into a json or bson representation suitable for use with
@@ -29,7 +38,9 @@ type Author struct {
 	CollectiveName string
 }
 
-// Article represents all of the retained
+// Article represents all of the retained information
+// from a PubmedArticle entry in an xml record
+//
 type Article struct {
 	PMID     string
 	Title    string
@@ -38,4 +49,117 @@ type Article struct {
 	Issue    PubInfo
 	Authors  []Author
 	Keywords []string
+}
+
+func parsePubInfo(article *Article, doc *xmlquery.Node) {
+	volume, _ := getText(doc, "//JournalIssue/Volume")
+	issue, _ := getText(doc, "//JournalIssue/Issue")
+	date, _ := getText(doc, "//PubDate//MedlineDate")
+	if date == "" {
+		date, _ = getText(doc, "//PubDate//Year")
+	}
+	pagination, _ := getText(doc, "//Pagination/MedlinePgn")
+	article.Issue.Date = date
+	article.Issue.Pages = pagination
+	article.Issue.Issue = issue
+	article.Issue.Vol = volume
+}
+
+// parseAbstract loads abstract information even if it is
+// stored in multiple parts
+func parseAbstract(article *Article, nodes []*xmlquery.Node) {
+	buffer := strings.Builder{}
+	for _, node := range nodes {
+		buffer.WriteString(node.InnerText())
+		buffer.WriteString("\n")
+	}
+	article.Abstract = buffer.String()
+}
+
+// parseAuthors populates the article.Authors slice corresponding to the
+// list of authors in the xml
+func parseAuthors(article *Article, doc *xmlquery.Node) {
+	nodes, _ := xmlquery.QueryAll(doc, "//AuthorList/Author")
+	for _, node := range nodes {
+		author := Author{}
+		author.FirstName, _ = getText(node, "//ForeName")
+		author.LastName, _ = getText(node, "//LastName")
+		author.Initials, _ = getText(node, "//Initials")
+		author.CollectiveName, _ = getText(node, "//CollectiveName")
+		author.Affiliation, _ = getText(node, "//AffiliationInfo/Affiliation")
+		article.Authors = append(article.Authors, author)
+	}
+}
+
+// parseKeywords populates the article.Keywords slice corresponding
+// to the accepted keywords in the xml record
+func parseKeywords(article *Article, doc *xmlquery.Node) {
+	kws := []string{}
+	nodes, _ := xmlquery.QueryAll(doc, "//MeshHeading")
+	for _, node := range nodes {
+		item, _ := getText(node, "//DescriptorName")
+		if len(item) > 0 {
+			kws = append(kws, item)
+		}
+	}
+	nodes, _ = xmlquery.QueryAll(doc, "//Keyword")
+	for _, node := range nodes {
+		kw := node.InnerText()
+		kws = append(kws, kw)
+	}
+	article.Keywords = append(article.Keywords, kws...)
+}
+
+// textOne attempts to get a single element from the document
+// path is of the form "//PMID"
+// retrurns "" if no data is found for this path
+// otherwise returns the InnerText of the node corresponding
+// to the path
+func getText(doc *xmlquery.Node, path string) (string, error) {
+	node, err := xmlquery.Query(doc, path)
+	if err != nil {
+		return "", err
+	}
+	if node == nil {
+		return "", err
+	}
+	return node.InnerText(), nil
+}
+
+// ParseXML reads the provided XML and attempts
+// to populate an Article structure from it.
+// Returns the Article
+//
+func ParseXML(xml io.Reader) (Article, error) {
+	dbg.Printf("Entering ParseXML")
+	result := Article{}
+	doc, err := xmlquery.Parse(xml)
+	if err != nil {
+		return result, err
+	}
+	pmid, err := getText(doc, "//PMID")
+	if err != nil {
+		return result, err
+	}
+	// pmid is a required field
+	if len(pmid) == 0 {
+		return result, fmt.Errorf("Unable to find PMID")
+	}
+	result.PMID = pmid
+	// assume that if there is a PMID, the xml record is valid so skip err check
+	result.Title, _ = getText(doc, "//ArticleTitle")
+	abstractNodes, _ := xmlquery.QueryAll(doc, "//Abstract/AbstractText")
+	if abstractNodes == nil {
+		result.Abstract, _ = getText(doc, "//Abstract")
+	} else {
+		parseAbstract(&result, abstractNodes)
+	}
+	parsePubInfo(&result, doc)
+	result.Journal.Title, _ = getText(doc, "//Journal/Title")
+	result.Journal.ISOAbbreviation, _ = getText(doc, "//ISOAbbreviation")
+	parseAuthors(&result, doc)
+	parseKeywords(&result, doc)
+
+	dbg.Printf("Leaving ParseXML")
+	return result, nil
 }
--- pubmed/pubmed_test.go
+++ pubmed/pubmed_test.go
@@ -57,3 +57,82 @@ func TestFindPMID(t *testing.T) {
 		fmt.Printf("abstract text element: %s\n", t.InnerText())
 	}
 }
+
+func TestParseXML(t *testing.T) {
+	data, err := ioutil.ReadFile("testdata/asthma_article_one.xml")
+	check(err)
+	xml := strings.NewReader(string(data))
+	article, err := ParseXML(xml)
+	check(err)
+	if article.PMID != "32374540" {
+		t.Errorf("Incorrect PMID: %s", article.PMID)
+	}
+	if !strings.HasPrefix(article.Title, "[Asthma and COPD") {
+		t.Errorf("Incorrect title: %s", article.Title)
+	}
+	if !strings.Contains(article.Abstract, "Numerous patients with asthma") {
+		t.Errorf("Incorrect abstract: %s ", article.Abstract)
+	}
+	pinfo := article.Issue
+	if pinfo.Vol != "16" {
+		t.Errorf("Incorrect volume number: %s", pinfo.Vol)
+	}
+	if article.Journal.Title != "Revue medicale suisse" {
+		t.Errorf("Wrong journal title: %s", article.Journal.Title)
+	}
+	if article.Journal.ISOAbbreviation != "Rev Med Suisse" {
+		t.Errorf("Wrong journal iso: %s", article.Journal.ISOAbbreviation)
+	}
+	if article.Authors[0].LastName != "Daccord" {
+		t.Errorf("Wrong last name: %s", article.Authors[0].LastName)
+	}
+	if article.Authors[0].FirstName != "Cécile" {
+		t.Errorf("Wrong first name: %s", article.Authors[0].FirstName)
+	}
+	if article.Keywords[3] != "Humans" {
+		t.Errorf("Wrong keyword: %s", article.Keywords[3])
+	}
+}
+
+func TestParseXMLTwo(t *testing.T) {
+	data, err := ioutil.ReadFile("testdata/asthma_article_two.xml")
+	check(err)
+	xml := strings.NewReader(string(data))
+	article, err := ParseXML(xml)
+	check(err)
+	if article.PMID != "31747880" {
+		t.Errorf("Incorrect PMID: %s", article.PMID)
+	}
+	if !strings.HasPrefix(article.Title, "The role of secreted Hsp90") {
+		t.Errorf("Incorrect title: %s", article.Title)
+	}
+	if !strings.Contains(article.Abstract, "The dysfunction of airway") {
+		t.Errorf("Incorrect abstract: %s ", article.Abstract)
+	}
+	pinfo := article.Issue
+	if pinfo.Vol != "19" {
+		t.Errorf("Incorrect volume number: %s", pinfo.Vol)
+	}
+	if article.Journal.Title != "BMC pulmonary medicine" {
+		t.Errorf("Wrong journal title: %s", article.Journal.Title)
+	}
+	if article.Journal.ISOAbbreviation != "BMC Pulm Med" {
+		t.Errorf("Wrong journal iso: %s", article.Journal.ISOAbbreviation)
+	}
+	if article.Authors[0].LastName != "Ye" {
+		t.Errorf("Wrong last name: %s", article.Authors[0].LastName)
+	}
+	if article.Authors[0].FirstName != "Cuiping" {
+		t.Errorf("Wrong first name: %s", article.Authors[0].FirstName)
+	}
+	if article.Keywords[3] != "Cadherins" {
+		t.Errorf("Wrong keyword: %s", article.Keywords[3])
+	}
+	i := len(article.Keywords) - 1
+	if article.Keywords[i] != "Secreted Hsp90α" {
+		t.Errorf("Wrong keyword: %s", article.Keywords[i])
+	}
+	if article.Keywords[i-1] != "HDM" {
+		t.Errorf("Wrong keyword: %s", article.Keywords[i-1])
+	}
+}
